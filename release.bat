6b204bc071a6d120ef84d2ecdbae03b6f263bb8b
KaiN <kain@piwnica.ws>
2021-01-06 15:20:30 +0100

    removed old zip generation task, 'cuz cmake handles it for some time now

--- release.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mkdir game
-copy build\germz.exe game
-REM copy germz.info game
-xcopy build\data game\data /e /i /h
-mkdir game\s
-echo germz.exe > game\s\startup-sequence
-exe2adf data\germz.exe -l "GermZ" -a "germz.adf" -0 -d game
-rmdir game /s /q
